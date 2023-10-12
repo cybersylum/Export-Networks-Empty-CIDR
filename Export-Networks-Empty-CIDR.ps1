8fe937c670040644a59dc8f9bdbcb3213e86d149
Arron King <5750297+cybersylum@users.noreply.github.com>
2023-10-12 10:54:21 -0400

    Significant re-write
    Script now
    * reads list of Cloud Accounts and prompts for user choice
    * uses powershell invoke-restmethod instead of PowervRA tooling to avoid 100 row return limit

--- Export-Networks-Empty-CIDR.ps1
+++ Export-Networks-Empty-CIDR.ps1
@@ -1,33 +1,145 @@
-#Exports list of Newtorks that do not have CIDR filled out
+<#
+Export-Networks-Empty-CIDR.ps1
+
+This script will export a list of vSphere Networks that have been discovered by Aria Automation 
+which have not been configured with IP Information.
+
+The script provides a prompt to allow a choice of all networks or networks associated with a 
+specific Cloud Account.
+
+Disclaimer:  This script was obtained from https://github.com/cybersylum
+  * You are free to use or modify this code for your own purposes.
+  * No warranty or support for this code is provided or implied.  
+  * Use this at your own risk.  
+  * Testing is highly recommended.
+#>
+
 
 # define vars for environment
+$vRAServer = "vra8.domain.com"
+$vRAUser = "user@domain.com"
 $DateStamp=Get-Date -format "yyyyMMdd"
 $TimeStamp=Get-Date -format "hhmmss"
 $ExportFile = "empty-cidr-networks-$DateStamp-$TimeStamp.csv"
-$vRAServer = "vra8.cybersylum.com"
-$vRAUser = "kinger@cybersylum.com"
 
+#QueryLimit is used to control the max rows returned by invoke-restmethod (which has a default of 100)
+$QueryLimit=9999
+
+$Error.clear()
+
+Write-Host "Connecting to Aria Automation -  $vRAServer as $vRAUser"
 $vRA=connect-vraserver -server $vRAServer -Username "$vRAUser" -IgnoreCertRequirements
 if ($null -eq $vRA) {
     write-host "Unable to connect to vRA Server '$vRAServer'..."
     exit
 }
 
-$EmptyNetworks = ""
-$Networks = (Invoke-vRARestMethod -Method GET -URI "/iaas/api/networks" -WebRequest).content | ConvertFrom-JSON -AsHashtable
+#Grab the bearer token for use with invoke-restmethod (which is needed for queries with more than 100 results)
+$APItoken= $vRA.token | ConvertTo-SecureString -AsPlainText -Force
 
-write-host "Exporting list of Networks with no CIDR defined to $ExportFile"
+$Body = @{
+    '$top' = $QueryLimit
+}
+$APIparams = @{
+    Method = "GET"
+    Uri = "https://$vRAServer/iaas/api/cloud-accounts"
+    Authentication = "Bearer"
+    Token = $APItoken
+    Body = $Body
+}
 
-foreach ($Network in $Networks.content) {
-   # write-host $Network.name " - " $Network.cidr
-    if ($Network.cidr.length -eq "") {
-        #write-host $Network.name
-        $EmptyNetworks += $Network.name
-        $EmptyNetworks += "`n"
+try{
+    $CloudAccounts = (Invoke-RestMethod @APIparams -SkipCertificateCheck).Content
+} catch {
+    Write-Host $("    Unable to get Cloud Accounts from Aria Automation")
+    Write-Host $Error
+    Write-Host $Error[0].Exception.GetType().FullName
+}
+
+Write-Host "Cloud Accounts found - " $CloudAccounts.Count
+Write-Host ""
+Write-Host "Choose a Cloud Account to use for Network Export:"
+$Index=0
+foreach ($Account in $CloudAccounts) {
+    Write-Host "    " $Index " - " $Account.Name " ("$Account.id")"
+    $Index++
+}
+Write-Host "   99 - All Cloud Accounts"
+write-host ""
+$Choice= Read-host -Prompt 'Enter selection and hit <ENTER> or just <ENTER> to quit'
+
+if ($Choice.length -eq 0) {
+    exit
+}
+
+try {
+    $Selection = [int]$Choice
+}
+catch {
+    write-host "$Choice is not a valid selection- exiting..."
+    exit
+}
+
+if ($Selection -ne 99) {
+    $InRange = $Selection -In 0..($Index-1)
+    if (-Not $InRange) {
+        write-host "$Choice is not a valid selection- exiting..."
+        exit
     }
 }
+
+if ($Selection -eq 99) {
+    $DisplayCloudAccountName = "All Cloud Accounts"
+    $CloudAccountID = ""
+} else {
+    $DisplayCloudAccountName = $CloudAccounts[$Selection].name
+    $CloudAccountID = $CloudAccounts[$Selection].id
+}
+
+#Get All Networks Discovered by vRA - so we can filter later if necessary
+$Body = @{
+    '$top' = $QueryLimit
+}
+$APIparams = @{
+    Method = "GET"
+    Uri = "https://$vRAServer/iaas/api/fabric-networks-vsphere"
+    Authentication = "Bearer"
+    Token = $APItoken
+    Body = $Body
+}
+
+try {
+    $Networks = (Invoke-RestMethod @APIparams -SkipCertificateCheck).content
+} catch {
+    Write-Host $("    Unable to get networks from vRA")
+    Write-Host $Error
+    Write-Host $Error[0].Exception.GetType().FullName
+}
+
+write-host "Networks discovered by vRA - " $Networks.count
+
+if ($CloudAccountID -eq "") {
+    #Use All Networks discovered by vRA
+    $FilteredNetworks = $Networks    
+} else {
+    #Filter to matching CloudAccount
+    $FilteredNetworks = $Networks | where-object -Property cloudAccountIds -eq $CloudAccountID
+}
+
+$EmptyNetworks = @()
+Write-Host "Scanning networks in $DisplayCloudAccountName for missing IP Info - " $FilteredNetworks.Count 
+foreach ($ThisNetwork in $FilteredNetworks) {
+   #write-host $ThisNetwork.name " - " $ThisNetwork.cidr
+    if ($ThisNetwork.cidr.length -eq "") {
+        #write-host $ThisNetwork.name
+        $EmptyNetworks += $ThisNetwork.name
+    }
+}
+
+Write-host "Networks with missing IP info - " $EmptyNetworks.count
  
 $EmptyNetworks | out-file -filepath $ExportFile
 
 # Clean up
+write-host "list exported to $ExportFile"
 Disconnect-vRAServer -Confirm:$false
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Export-Networks-Empty-CIDR
 
-Disclaimer
-  * You are free to use or modify this code for your own purposes.
-  * Warranty and/or support for this code is not provided or implied.  Use this at your own risk.  Testing is highly recommended.
+Disclaimer:  This script was obtained from https://github.com/cybersylum<br>
+<ul>
+<li>You are free to use or modify this code for your own purposes</li>
+<li>No warranty or support for this code is provided or implied</li>  
+<li>Use this at your own risk</li>
+<li>Testing is highly recommended.
+</ul>
+<br>
+<P>This script will export a list of vSphere Networks that have been discovered by Aria Automation which have not been configured with IP Information.</P>
+
+<P>The script provides a prompt to allow a choice of all networks or networks associated with a specific Cloud Account.</P>
+
+<p>All scripts require Poweshell and PowerVRA 6.x+ - https://github.com/jakkulabs/PowervRA</p>
+
